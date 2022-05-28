5a47008582e4932ef27f8d7ebe21d6486d1e062b
Dennis Downer <downerdm@gmail.com>
2022-05-28 14:15:08 -0400

    Fixed dependencies

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,47 +1,26 @@
 import React from 'react';
-import emailEmoji from "../assets/images/email_emoji.png";
+import {Navbar, Nav, Stack} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import {
+    faEnvelope,
+    } from '@fortawesome/free-solid-svg-icons';
 
-export default function Footer() {
-  return (
-    <section class="SocialsListWrapper_socialIconList__SDG9X SocialsListWrapper_socialIconListWrap__uZu_z SocialsListWrapper_socialIconListCenter__aOqGM">
-    <div class="SocialItemWrapper_socialItemWrapper__lTD8X">
-      <a class="BaseSocialIcon_baseSocialIcon__qty1p" href="https://youtube.com/channel/UCh3ZdqVckEaAxeTXec3HDzQ?utm_source=hoobe&amp;utm_medium=page" target="_blank" rel="noreferrer" data-agency-bg-color="true">
-          <svg class="" width="24px" height="24px" viewBox="-1 -1 26 26" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" fill="var(--social-icon-color)">
-            </path>
-            </svg>
-      </a>
-    </div>
-    
-    <div class="SocialItemWrapper_socialItemWrapper__lTD8X">
-      <a class="BaseSocialIcon_baseSocialIcon__qty1p" href="https://www.youngla.com/?utm_source=hoobe&amp;utm_medium=page" target="_blank" rel="noreferrer" data-agency-bg-color="true">
-        <svg class="" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M4 9.00022H5.99978C6.00007 9.36676 6.20262 9.71956 6.55279 9.89465C7.04676 10.1416 7.64744 9.94141 7.89443 9.44743L8.11803 9.00022H15.882L16.1056 9.44743C16.3526 9.94141 16.9532 10.1416 17.4472 9.89465C17.7974 9.71956 17.9999 9.36676 18.0002 9.00022H20V11.0002H4V9.00022ZM17.118 7.00022H20C21.1046 7.00022 22 7.89565 22 9.00022V11.0002C22 11.8402 21.4821 12.5593 20.7482 12.8556C20.7516 12.9033 20.7534 12.9516 20.7534 13.0002C20.7534 13.1461 20.7374 13.2916 20.7058 13.4341L19.1502 20.4341C18.9469 21.3492 18.1352 22.0002 17.1978 22.0002H6.80217C5.86477 22.0002 5.05315 21.3492 4.8498 20.4341L3.29424 13.4341C3.25095 13.2392 3.23767 13.0447 3.25134 12.8554C2.51767 12.559 2 11.8401 2 11.0002V9.00022C2 7.89565 2.89543 7.00022 4 7.00022H6.88197L9.10557 2.55301C9.35256 2.05903 9.95323 1.8588 10.4472 2.10579C10.9412 2.35278 11.1414 2.95345 10.8944 3.44743L9.11803 7.00022H14.882L13.1056 3.44743C12.8586 2.95345 13.0588 2.35278 13.5528 2.10579C14.0468 1.8588 14.6474 2.05903 14.8944 2.55301L17.118 7.00022ZM5.24662 13.0002H18.7534L17.1978 20.0002H6.80217L5.24662 13.0002Z" fill="var(--social-icon-color)">
-          </path>
-        </svg>
-      </a>
-    </div>
-    <div class="SocialItemWrapper_socialItemWrapper__lTD8X">
-      <a class="BaseSocialIcon_baseSocialIcon__qty1p" href="https://instagram.com/downerdm?utm_source=hoobe&amp;utm_medium=page" target="_blank" rel="noreferrer" data-agency-bg-color="true">
-        <svg class="" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M2 6C2 4.93913 2.42143 3.92172 3.17157 3.17157C3.92172 2.42143 4.93913 2 6 2H18C19.0609 2 20.0783 2.42143 20.8284 3.17157C21.5786 3.92172 22 4.93913 22 6V18C22 19.0609 21.5786 20.0783 20.8284 20.8284C20.0783 21.5786 19.0609 22 18 22H6C4.93913 22 3.92172 21.5786 3.17157 20.8284C2.42143 20.0783 2 19.0609 2 18V6ZM6 4C5.46957 4 4.96086 4.21071 4.58579 4.58579C4.21071 4.96086 4 5.46957 4 6V18C4 18.5304 4.21071 19.0391 4.58579 19.4142C4.96086 19.7893 5.46957 20 6 20H18C18.5304 20 19.0391 19.7893 19.4142 19.4142C19.7893 19.0391 20 18.5304 20 18V6C20 5.46957 19.7893 4.96086 19.4142 4.58579C19.0391 4.21071 18.5304 4 18 4H6ZM12 9C11.2044 9 10.4413 9.31607 9.87868 9.87868C9.31607 10.4413 9 11.2044 9 12C9 12.7956 9.31607 13.5587 9.87868 14.1213C10.4413 14.6839 11.2044 15 12 15C12.7956 15 13.5587 14.6839 14.1213 14.1213C14.6839 13.5587 15 12.7956 15 12C15 11.2044 14.6839 10.4413 14.1213 9.87868C13.5587 9.31607 12.7956 9 12 9ZM7 12C7 10.6739 7.52678 9.40215 8.46447 8.46447C9.40215 7.52678 10.6739 7 12 7C13.3261 7 14.5979 7.52678 15.5355 8.46447C16.4732 9.40215 17 10.6739 17 12C17 13.3261 16.4732 14.5979 15.5355 15.5355C14.5979 16.4732 13.3261 17 12 17C10.6739 17 9.40215 16.4732 8.46447 15.5355C7.52678 14.5979 7 13.3261 7 12ZM17.5 8C17.8978 8 18.2794 7.84196 18.5607 7.56066C18.842 7.27936 19 6.89782 19 6.5C19 6.10218 18.842 5.72064 18.5607 5.43934C18.2794 5.15804 17.8978 5 17.5 5C17.1022 5 16.7206 5.15804 16.4393 5.43934C16.158 5.72064 16 6.10218 16 6.5C16 6.89782 16.158 7.27936 16.4393 7.56066C16.7206 7.84196 17.1022 8 17.5 8Z" fill="var(--social-icon-color)">
-          </path>
-        </svg>
-      </a>
-    </div>
-    
-    <div class="SocialItemWrapper_socialItemWrapper__lTD8X">
-      <a class="BaseSocialIcon_baseSocialIcon__qty1p" href="https://discord.gg/K4Kt7z7y?utm_source=hoobe&amp;utm_medium=page" target="_blank" rel="noreferrer" data-agency-bg-color="true">
-        <svg class="" width="18px" height="18px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path fill="var(--social-icon-color)" fill-rule="evenodd" clip-rule="evenodd" d="M20.222 0c1.406 0 2.54 1.137 2.607 2.475V24l-2.677-2.273-1.47-1.338-1.604-1.398.67 2.205H3.71c-1.402 0-2.54-1.065-2.54-2.476V2.48C1.17 1.142 2.31.003 3.715.003h16.5L20.222 0zm-6.118 5.683h-.03l-.202.2c2.073.6 3.076 1.537 3.076 1.537-1.336-.668-2.54-1.002-3.744-1.137-.87-.135-1.74-.064-2.475 0h-.2c-.47 0-1.47.2-2.81.735-.467.203-.735.336-.735.336s1.002-1.002 3.21-1.537l-.135-.135s-1.672-.064-3.477 1.27c0 0-1.805 3.144-1.805 7.02 0 0 1 1.74 3.743 1.806 0 0 .4-.533.805-1.002-1.54-.468-2.14-1.404-2.14-1.404s.134.066.335.2h.06c.03 0 .044.015.06.03v.006c.016.016.03.03.06.03.33.136.66.27.93.4.466.202 1.065.403 1.8.536.93.135 1.996.2 3.21 0 .6-.135 1.2-.267 1.8-.535.39-.2.87-.4 1.397-.737 0 0-.6.936-2.205 1.404.33.466.795 1 .795 1 2.744-.06 3.81-1.8 3.87-1.726 0-3.87-1.815-7.02-1.815-7.02-1.635-1.214-3.165-1.26-3.435-1.26l.056-.02zm.168 4.413c.703 0 1.27.6 1.27 1.335 0 .74-.57 1.34-1.27 1.34-.7 0-1.27-.6-1.27-1.334.002-.74.573-1.338 1.27-1.338zm-4.543 0c.7 0 1.266.6 1.266 1.335 0 .74-.57 1.34-1.27 1.34-.7 0-1.27-.6-1.27-1.334 0-.74.57-1.338 1.27-1.338z">
-          </path>
-        </svg>
-      </a>
-    </div>
-  </section>
-  
-//   <div class="text-center p-3">
-//   © 2022 Copyright: Dennis M. Downer
-// </div>);
-  )
-}
+export default function Footer (){
+    return (
+      <footer class="footer">
+        <Navbar fixed="bottom" bg="dark" variant="dark" style={{ position: 'sticky', bottom: 0, zIndex: 1, borderRadius: 2 }}>
+        <Nav className='me-auto ms-auto'>
+          <Stack className='text-center'>
+            <Stack direction="horizontal">
+              <Nav.Link href="mailto:downerdm@gmail.com"><FontAwesomeIcon icon={faEnvelope} color="#fff" /></Nav.Link>
+              <Nav.Link href="https://github.com/downerdm"><FontAwesomeIcon icon={faGithub} color="#fff" /></Nav.Link>
+              <Nav.Link href="https://www.linkedin.com/in/downerdm"><FontAwesomeIcon icon={faLinkedin} class="fa-linkedin" /></Nav.Link>
+            </Stack>
+            <Nav.Link className='text-muted p-0 m-0'> © {new Date().getFullYear()} Dennis M. Downer</Nav.Link>
+          </Stack>
+        </Nav>
+      </Navbar>
+    </footer>    
+      )
+    };
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -28,12 +28,9 @@ export default function PortfolioContainer() {
 
   return (
     <div>
-      {/* We are passing the currentPage from state and the function to update it */}
       <Header />
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* Here we are calling the renderPage method which will return a component  */}
       {renderPage()}
-      <hr />
       <Footer />
     </div>
   );
