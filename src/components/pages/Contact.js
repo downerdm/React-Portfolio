86193cd2aca4f139806f4f6d15eb4032cde92609
Dennis Downer <downerdm@gmail.com>
2022-06-10 12:00:02 -0400

    added utils folder

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../assets/css/style.css';
+import signUpCheck from '../../utils/checks';
 
 export default function Contact() {
   return (
---
+++ src/utils/checks.js
@@ -0,0 +1,27 @@
+let errMsg;
+
+const validateSignUp = (email, password, alert) => {
+  // targeting all form fields
+  const emailInput = document.querySelector("#email");
+  const passwordInput = document.querySelector("#password");
+  
+  if (!email) {
+    alert.error("Please input your email address");
+    emailInput.focus();
+    errMsg = false;
+  } else if (password === "") {
+    alert.error("Please provide your password!");
+    passwordInput.focus();
+    errMsg = false;
+  } else if (password.value <= 7) {
+    alert.error("Your password must have 8 characters or greater");
+    passwordInput.focus();
+    errMsg = false;
+  } else {
+    alert.success("You've signed up successfully. Proceed to login");
+    errMsg = true;
+  }
+  return errMsg;
+};
+
+export default signUpCheck;
